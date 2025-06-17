34fd543fc0cfd932cbc874b0a47fab9fa3896680
mrMcAko <alechacin08@gmail.com>
2025-06-17 14:03:44 -0600

    fix: Modificamos rutas para obtener todas las subastas

--- backend/routes/auctionRoutes.js
+++ backend/routes/auctionRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const auctionRoutes = express.Router();
 const auctionController = require('../controllers/auctionController');
 
-auctionRoutes.get('/', auctionController.getAuctions);
+auctionRoutes.get('/', auctionController.getAllAuctions);
 auctionRoutes.get('/:id', auctionController.getAuction);
 auctionRoutes.post('/', auctionController.createAuction);
 auctionRoutes.put('/:id', auctionController.updateAuction);
